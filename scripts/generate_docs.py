54efb402041e042e9442ad5db44c7a1b7960a9e1
Diego Temkin <diego.temkin@dot.gov>
2024-06-12 14:24:26 -0400

    add automated docs generator (iteration 1)

---
+++ scripts/generate_docs.py
@@ -0,0 +1,33 @@
+import json
+from frictionless import Package, Schema
+import pathlib
+from os import listdir
+from os.path import isfile, join, split
+from pprint import pprint
+
+
+current_path = pathlib.Path(__file__).parent.resolve()
+specs_path = current_path / '..' / 'spec'
+docs_path = current_path / '..' / 'docs' / 'spec'
+
+onlyfiles = [specs_path / f for f in listdir(specs_path) if isfile(join(specs_path, f))]
+
+for file in onlyfiles:
+    if "schema" in str(file):
+        schema = Schema.from_descriptor(file.absolute().as_posix())
+        schema.to_markdown((docs_path / f"{split(file)[-1].split('.')[0]}.md").absolute().as_posix(), True)
+    elif "datapackage" in str(file):
+        with open(file) as f:
+            json_data = json.load(f)
+        for index, resource in enumerate(json_data['resources']):
+            if type(resource['schema']) is str:
+                with open((specs_path / resource['schema'])) as r:
+                    json_data['resources'][index]['schema'] = json.load(r)
+        package = Package(json_data)
+        er_diagram = package.to_er_diagram((docs_path / "diagram.dot").absolute().as_posix())
+
+        # package.image = er_diagram
+        # pprint(package)
+
+        package.to_markdown((docs_path / "README.md").absolute().as_posix())
+        
