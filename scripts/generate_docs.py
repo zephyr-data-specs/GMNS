87eaafea9ffd2c4ca436045cecc151b9f1df4c89
Diego Temkin <diego.temkin@dot.gov>
2024-06-18 10:29:56 -0400

    formatting

--- scripts/generate_docs.py
+++ scripts/generate_docs.py
@@ -13,7 +13,9 @@ docs_path = script_path / ".." / "docs" / "spec"
 db_path = script_path / ".." / "usage" / "database"
 
 # Get all files in spec_path
-spec_files = [specs_path / f for f in listdir(specs_path) if isfile(join(specs_path, f))]
+spec_files = [
+    specs_path / f for f in listdir(specs_path) if isfile(join(specs_path, f))
+]
 
 for spec_file in spec_files:
     # Combine every schema file into one datapackage
@@ -60,21 +62,26 @@ for spec_file in spec_files:
             if type(resource.path) is str:
                 file_to_create = resource.path
                 if not os.path.exists(file_to_create):
-                    with open(file_to_create, "a+"): files_to_delete.append(file_to_create)
+                    with open(file_to_create, "a+"):
+                        files_to_delete.append(file_to_create)
 
         # Use https://alpha.sqliteviewer.app/ for verification!
         os.remove(db_path / "gmns.sqlite")
-        create_db = package.publish(f"sqlite:///{(db_path / "gmns.sqlite").absolute().as_posix()}")
+        create_db = package.publish(
+            "sqlite:///" + (db_path / "gmns.sqlite").absolute().as_posix()
+        )
 
         # Get list of every table and its schema
-        connection = sqlite3.connect(f"{db_path.absolute().as_posix()}/gmns.sqlite")
+        connection = sqlite3.connect(db_path / "gmns.sqlite")
         cursor = connection.cursor()
         cursor.execute("SELECT name, sql FROM sqlite_master WHERE type='table';")
         list_of_tables = cursor.fetchall()
         for table_name, table_sql in list_of_tables:
             with open(db_path / f"{table_name}.sql", "w") as table_file:
                 # Add "IF NOT EXISTS" condition
-                table_sql = table_sql.replace("CREATE TABLE", "CREATE TABLE IF NOT EXISTS")
+                table_sql = table_sql.replace(
+                    "CREATE TABLE", "CREATE TABLE IF NOT EXISTS"
+                )
                 table_file.write(table_sql)
         cursor.close()
         connection.close()
