25db15db7b5d142402ba00dfacd16e735030e0c8
dtemkin-volpe <171841957+dtemkin-volpe@users.noreply.github.com>
2024-06-24 19:15:23 +0000

    Update documentation

--- usage/database/link.sql
+++ usage/database/link.sql
@@ -9,10 +9,10 @@ CREATE TABLE IF NOT EXISTS link (
 	parent_link_id TEXT, 
 	dir_flag INTEGER, 
 	length FLOAT CHECK (length >= 0), 
-	grade FLOAT CHECK (grade <= 100) CHECK (grade >= -100), 
+	grade FLOAT CHECK (grade >= -100) CHECK (grade <= 100), 
 	facility_type TEXT, 
 	capacity FLOAT CHECK (capacity >= 0), 
-	free_speed FLOAT CHECK (free_speed >= 0) CHECK (free_speed <= 200), 
+	free_speed FLOAT CHECK (free_speed <= 200) CHECK (free_speed >= 0), 
 	lanes INTEGER CHECK (lanes >= 0), 
 	bike_facility VARCHAR(22), 
 	ped_facility VARCHAR(14), 
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS link (
 	row_width FLOAT CHECK (row_width >= 0), 
 	PRIMARY KEY (link_id), 
 	FOREIGN KEY(from_node_id) REFERENCES node (node_id), 
-	FOREIGN KEY(geometry_id) REFERENCES geometry (geometry_id), 
 	FOREIGN KEY(to_node_id) REFERENCES node (node_id), 
-	FOREIGN KEY(parent_link_id) REFERENCES link (link_id)
+	FOREIGN KEY(parent_link_id) REFERENCES link (link_id), 
+	FOREIGN KEY(geometry_id) REFERENCES geometry (geometry_id)
 )
--- usage/database/signal_phase_mvmt.sql
+++ usage/database/signal_phase_mvmt.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS signal_phase_mvmt (
 	link_id TEXT, 
 	protection VARCHAR(9), 
 	PRIMARY KEY (signal_phase_mvmt_id), 
-	FOREIGN KEY(timing_phase_id) REFERENCES signal_timing_phase (timing_phase_id), 
+	FOREIGN KEY(mvmt_id) REFERENCES movement (mvmt_id), 
 	FOREIGN KEY(link_id) REFERENCES link (link_id), 
-	FOREIGN KEY(mvmt_id) REFERENCES movement (mvmt_id)
+	FOREIGN KEY(timing_phase_id) REFERENCES signal_timing_phase (timing_phase_id)
 )
