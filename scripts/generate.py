dc267b78bd28f1997e74327b85ab5bb1eb96542f
Diego Temkin <diego.temkin@dot.gov>
2024-07-18 10:13:17 -0400

    making sure everything works...

--- scripts/generate.py
+++ scripts/generate.py
@@ -1,14 +1,14 @@
 from shared import DOCS_PATH, gmns
 
 if __name__ == "__main__":
-    with open(DOCS_PATH / "README.md") as old_file:
-        old_docs = old_file.read().split("\n")
+    # with open(DOCS_PATH / "README.md") as old_file:
+    #     old_docs = old_file.read().split("\n")
 
     new_docs = gmns.generate_docs()
 
-    if old_docs == new_docs:
-        # Change in schema detected! Generating new database...
-        gmns.generate_db()
+    # if old_docs == new_docs:
+    # Change in schema detected! Generating new database...
+    gmns.generate_db()
 
     # with open(SCRIPT_PATH / "datapackage.json", "w+") as file:
     #     json.dump(gmns.json_data, file)
