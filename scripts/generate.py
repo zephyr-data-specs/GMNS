3ad99bac0c4ea3e8dab4ddddcfc22b5a3ebab467
Diego Temkin <diego.temkin@dot.gov>
2024-07-19 10:17:37 -0400

    dont generate db schema without changing spec

--- scripts/generate.py
+++ scripts/generate.py
@@ -1,14 +1,20 @@
+import shutil
+
 from shared import DOCS_PATH, gmns
 
 if __name__ == "__main__":
-    # with open(DOCS_PATH / "README.md") as old_file:
-    #     old_docs = old_file.read().split("\n")
+    # Get old docs
+    with open(DOCS_PATH / "README.md") as old_file:
+        old_docs = "".join(old_file.read().split("\n"))
 
-    new_docs = gmns.generate_docs()
+    # Delete old doc files
+    shutil.rmtree(DOCS_PATH)
 
-    # if old_docs == new_docs:
-    # Change in schema detected! Generating new database...
-    gmns.generate_db()
+    new_docs = "".join(gmns.generate_docs().split("\n"))
+
+    if old_docs != new_docs:
+        # Change in schema detected! Generating new database...
+        gmns.generate_db()
 
     # with open(SCRIPT_PATH / "datapackage.json", "w+") as file:
     #     json.dump(gmns.json_data, file)
